fe1395d9a3a02ea084606c589626b2d1e7cccea2
crajashekhar79 <crajashekhar79@gmail.com>
2026-07-05 18:53:01 +0530

    Did some changes in some files

--- 01-Data-Structures-Algorithms/Largest_number.java
+++ 01-Data-Structures-Algorithms/Largest_number.java
@@ -1,8 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Largest_number{
 
 
     public static void main(String[] args){
-        System.out.println("This Program prints the largest value in a given array.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:-");
+        int num = sc.nextInt();
+
+        int c = 2;
+        while( (c * c) < num ){
+
+            if(num % c ==0){
+                System.out.println("Given number is not a prime");
+                break;
+            }
+           c+=1;
+
+
+        }
+        System.out.println();
+
 
 
     }
